185ef9bad75288e093df25e7be99966cd44c688b
321thijs123 <thijs.hof@home.nl>
2022-12-19 13:09:20 +0100

    Implemented STL Export

--- day18/day18-part2.cpp
+++ day18/day18-part2.cpp
@@ -3,6 +3,7 @@
 #include "../readers.h";
 
 #include <iostream>
+#include <fstream>
 
 void day18P2::assignment(string filename) {
 	vector<vector<int>> cubes = readIntMatrix(filename);
@@ -39,6 +40,8 @@ void day18P2::assignment(string filename) {
 	flood(voxelmap, 0, 0, 0);
 
 	show(voxelmap);
+
+	generateSTL(voxelmap, '#');
 
 	cout << getArea(voxelmap, '~') << '\n';
 }
@@ -87,4 +90,61 @@ void day18P2::show(const vector<vector<vector<char>>> voxelmap) {
 		}
 		cout << "\n\n";
 	}
+}
+
+void day18P2::generateSTL(const vector<vector<vector<char>>> voxelmap, char c) {
+	ofstream file;
+
+	file.open("blob.stl", ios::trunc);
+
+	file << "solid \"Blob\"" << '\n';
+
+	for (int x = 0; x < voxelmap.size(); x++) {
+		for (int y = 0; y < voxelmap[0].size(); y++) {
+			for (int z = 0; z < voxelmap[0][0].size(); z++) {
+				if (voxelmap[x][y][z] == c) {
+					if (x > 0 && voxelmap[x - 1][y][z] != c) {
+						file << stlSquare(x, y, z, x, y + 1, z, x, y + 1, z + 1, x, y, z + 1, -1, 0, 0);
+					}
+					if (y > 0 && voxelmap[x][y - 1][z] != c) {
+						file << stlSquare(x, y, z, x + 1, y, z, x + 1, y, z + 1, x, y, z + 1, 0, -1, 0);
+					}
+					if (z > 0 && voxelmap[x][y][z - 1] != c) {
+						file << stlSquare(x, y, z, x, y + 1, z, x + 1, y + 1, z, x + 1, y, z, 0, 0, -1);
+					}
+					if (x < voxelmap.size() - 1 && voxelmap[x + 1][y][z] != c) {
+						file << stlSquare(x + 1, y, z, x + 1, y + 1, z, x + 1, y + 1, z + 1, x + 1, y, z + 1, 1, 0, 0);
+					}
+					if (y < voxelmap[0].size() - 1 && voxelmap[x][y + 1][z] != c) {
+						file << stlSquare(x, y + 1, z, x + 1, y + 1, z, x + 1, y + 1, z + 1, x, y + 1, z + 1, 0, 1, 0);
+					}
+					if (z < voxelmap[0][0].size() - 1 && voxelmap[x][y][z + 1] != c) {
+						file << stlSquare(x, y, z + 1, x, y + 1, z + 1, x + 1, y + 1, z + 1, x + 1, y, z + 1, 0, 0, 1);
+					}
+				}
+			}
+		}
+	}
+
+	file << "endsolid \"Blob\"" << '\n';
+
+	file.close();
+}
+
+string day18P2::stlSquare(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3, int x4, int y4, int z4, int n1, int n2, int n3) {
+	return
+		"facet normal " + to_string(n1) + ' ' + to_string(n2) + ' ' + to_string(n3) + '\n' +
+		"outer loop" + '\n' +
+		"vertex " + to_string(x1) + ' ' + to_string(y1) + ' ' + to_string(z1) + '\n' +
+		"vertex " + to_string(x2) + ' ' + to_string(y2) + ' ' + to_string(z2) + '\n' +
+		"vertex " + to_string(x3) + ' ' + to_string(y3) + ' ' + to_string(z3) + '\n' +
+		"endloop" + '\n' +
+		"endfacet" + "\n\n" +
+		"facet normal " + to_string(n1) + ' ' + to_string(n2) + ' ' + to_string(n3) + '\n' +
+		"outer loop" + '\n' +
+		"vertex " + to_string(x1) + ' ' + to_string(y1) + ' ' + to_string(z1) + '\n' +
+		"vertex " + to_string(x4) + ' ' + to_string(y4) + ' ' + to_string(z4) + '\n' +
+		"vertex " + to_string(x3) + ' ' + to_string(y3) + ' ' + to_string(z3) + '\n' +
+		"endloop" + '\n' +
+		"endfacet" + "\n\n";
 }
--- day18/day18-part2.h
+++ day18/day18-part2.h
@@ -10,4 +10,6 @@ namespace day18P2 {
 	void flood(vector<vector<vector<char>>>& voxelmap, const int x, const int y, const int z);
 	int getArea(const vector<vector<vector<char>>> voxelmap, char c);
 	void show(const vector<vector<vector<char>>> voxelmap);
+	void generateSTL(const vector<vector<vector<char>>> voxelmap, char c);
+	string stlSquare(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3, int x4, int y4, int z4, int n1, int n2, int n3);
 }
